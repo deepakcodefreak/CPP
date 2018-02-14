937df9850ec88d3b83c33fe29dad8b06153d2a20
deepakcodefreak <deepak19.engg@gmail.com>
2018-02-15 02:18:33 +0530

    14/2/2018

---
+++ setw_manipulator.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+
+int main()
+{
+    int m=2548,n=45,o=8;
+    cout<<"m="<<setw(6)<<m<<endl;
+    cout<<"n="<<setw(6)<<n<<endl;
+    cout<<"o="<<setw(6)<<o<<endl;
+    return 0;
+}
